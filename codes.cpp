4b89e2895a3a1fe7709be90123725d51b372bd03
ScorpioMiku <1056992492@qq.com>
2019-03-06 21:07:42 +0800

    1

--- codes.cpp
+++ codes.cpp
@@ -1,51 +1,79 @@
-﻿//在顺序表中查找某值，若没找到就插入
+﻿//合并两有序数组
 #include "pch.h"
 #include <iostream>
 
 void showArray(int as[], int length);
 
-void function(int *a, int target, int length)
+int getMid(int * a, int *b, int length)
 {
-	int star = 0, end = length - 1, index = 0;
-	while (star <= end)
+	////时间复杂度为O(N)
+	//int indexA = 0, indexB = 0, mid = 0;
+	//while (indexA + indexB < length)
+	//{
+	//	if (a[indexA] < b[indexB])
+	//	{
+	//		mid = a[indexA];
+	//		indexA++;
+	//	}
+	//	else
+	//	{
+	//		mid = a[indexB];
+	//		indexB++;
+	//	}
+	//}
+	//return mid;
+
+	int indexAStar = 0, indexBStar = 0, indexAEnd = length - 1, indexBEnd = length - 1;
+	int aMid = 0, bMid = 0;
+
+	while (indexAEnd != indexAStar || indexBEnd != indexBStar)
 	{
-		index = (star + end) / 2;
-		if (a[index] == target)
+		printf("a:%d,%d ; b:%d,%d\n", indexAStar, indexAEnd, indexBStar, indexBEnd);
+		aMid = (indexAStar + indexAEnd) / 2;
+		bMid = (indexBStar + indexBEnd) / 2;
+		if (a[aMid] == b[bMid])
 		{
-			if (index != length-1)
-			{
-				a[index] = a[index] + a[index + 1];
-				a[index + 1] = a[index] - a[index + 1];
-				a[index] = a[index] - a[index + 1];
-			}
-			break;
+			return a[aMid];
 		}
-		else if (a[index] > target)
+		else if (a[aMid] > b[bMid])
 		{
-			end = index - 1;
+			if (indexAEnd != indexAStar)
+			{
+				indexAEnd = aMid - 1;
+			}
+			if (indexBEnd != indexBStar)
+			{
+				indexBStar = bMid + 1;
+			}
 		}
 		else
 		{
-			star = index + 1;
-		}
-	}
-	printf("%d,%d\n", star, end);
-	if (star > end)
-	{
-		/*int i = 0;
-		for (i = length - 1; i > end; i--)
-		{
-			a[i + 1] = a[i];
-		}
-		a[i + 1] = target;*/
-	}
+			if (indexAEnd != indexAStar)
+			{
+				indexAStar = aMid + 1;
+			}
+			if (indexBEnd != indexBStar)
+			{
+				indexBEnd = bMid - 1;
+			}
 
+
+		}
+	}
+	if (a[aMid] < b[bMid])
+	{
+		return a[aMid];
+	}
+	else
+	{
+		return b[bMid];
+	}
 }
 int main()
 {
-	int a[9] = { 1,2,4,5,6,7,8,9,10 };
-	function(a, -1, 9);
-	showArray(a, 9);
+	int a[9] = { 0,0,0,0,0,0,0,0,0 };
+	int b[9] = { 2,4,6,8,10,12,14,16,18 };
+	printf("%d\n", getMid(a, b, 9));
 }
 
 void showArray(int as[], int length)
@@ -55,5 +83,3 @@ void showArray(int as[], int length)
 		printf("%d\t", as[i]);
 	}
 }
-
-
--- codes.md
+++ codes.md
@@ -221,10 +221,13 @@ void function(int *a, int target, int length)
 		index = (star + end) / 2;
 		if (a[index] == target)
 		{
-			a[index] = a[index] + a[index + 1];
-			a[index + 1] = a[index] - a[index + 1];
-			a[index] = a[index] - a[index + 1];
-			break;
+			if (index != length-1)
+			{
+				a[index] = a[index] + a[index + 1];
+				a[index + 1] = a[index] - a[index + 1];
+				a[index] = a[index] - a[index + 1];
+			}
+            break;
 		}
 		else if (a[index] > target)
 		{
@@ -267,7 +270,7 @@ void showArray(int as[], int length)
 ```
 
 ```c++
-//获取两有序数组的中位数
+//合并两有序数组
 #include "pch.h"
 #include <iostream>
 
@@ -275,25 +278,71 @@ void showArray(int as[], int length);
 
 int getMid(int * a, int *b, int length)
 {
-	int indexA = 0, indexB = 0, mid = 0;
-	while (indexA+indexB < length)
+	////时间复杂度为O(N)
+	//int indexA = 0, indexB = 0, mid = 0;
+	//while (indexA + indexB < length)
+	//{
+	//	if (a[indexA] < b[indexB])
+	//	{
+	//		mid = a[indexA];
+	//		indexA++;
+	//	}
+	//	else
+	//	{
+	//		mid = a[indexB];
+	//		indexB++;
+	//	}
+	//}
+	//return mid;
+
+	int indexAStar = 0, indexBStar = 0, indexAEnd = length - 1, indexBEnd = length - 1;
+	int aMid = 0, bMid = 0;
+
+	while (indexAEnd != indexAStar || indexBEnd != indexBStar)
 	{
-		if (a[indexA]<b[indexB])
+		printf("a:%d,%d ; b:%d,%d\n", indexAStar, indexAEnd, indexBStar, indexBEnd);
+		aMid = (indexAStar + indexAEnd) / 2;
+		bMid = (indexBStar + indexBEnd) / 2;
+		if (a[aMid] == b[bMid])
 		{
-			mid = a[indexA];
-			indexA++;
+			return a[aMid];
+		}
+		else if (a[aMid] > b[bMid])
+		{
+			if (indexAEnd != indexAStar)
+			{
+				indexAEnd = aMid - 1;
+			}
+			if (indexBEnd != indexBStar)
+			{
+				indexBStar = bMid + 1;
+			}
 		}
 		else
 		{
-			mid = a[indexB];
-			indexB++;
+			if (indexAEnd != indexAStar)
+			{
+				indexAStar = aMid + 1;
+			}
+			if (indexBEnd != indexBStar)
+			{
+				indexBEnd = bMid - 1;
+			}
 		}
 	}
-	return mid;
+	if (a[aMid] < b[bMid])
+	{
+		return a[aMid];
+	}
+	else
+	{
+		return b[bMid];
+	}
+	//时间复杂度为log2(n)
 }
 int main()
 {
-	int a[9] = { 1,1,1,1,1,1,1,1,1};
+	int a[9] = { 0,0,0,0,0,0,0,0,0 };
 	int b[9] = { 2,4,6,8,10,12,14,16,18 };
 	printf("%d\n", getMid(a, b, 9));
 }
