1013e8c093444c0bcb2173c95176c9b0c62f1cf8
ScorpioMiku <1056992492@qq.com>
2019-03-08 21:42:58 +0800

    1

--- codes.cpp
+++ codes.cpp
@@ -1,85 +1,49 @@
-﻿//合并两有序数组
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
+typedef int ElemType;
+typedef struct LNode {
+	ElemType value;
+	struct LNode * next;
+}LNode, *LinkList;
 
-void showArray(int as[], int length);
-
-int getMid(int * a, int *b, int length)
+LinkList onCreate(LinkList L)
 {
-	////时间复杂度为O(N)
-	//int indexA = 0, indexB = 0, mid = 0;
-	//while (indexA + indexB < length)
-	//{
-	//	if (a[indexA] < b[indexB])
-	//	{
-	//		mid = a[indexA];
-	//		indexA++;
-	//	}
-	//	else
-	//	{
-	//		mid = a[indexB];
-	//		indexB++;
-	//	}
-	//}
-	//return mid;
-
-	int indexAStar = 0, indexBStar = 0, indexAEnd = length - 1, indexBEnd = length - 1;
-	int aMid = 0, bMid = 0;
-
-	while (indexAEnd != indexAStar || indexBEnd != indexBStar)
+	LNode * node = NULL;
+	int x;
+	scanf_s("%d", &x);
+	if (L == NULL)
 	{
-		printf("a:%d,%d ; b:%d,%d\n", indexAStar, indexAEnd, indexBStar, indexBEnd);
-		aMid = (indexAStar + indexAEnd) / 2;
-		bMid = (indexBStar + indexBEnd) / 2;
-		if (a[aMid] == b[bMid])
-		{
-			return a[aMid];
-		}
-		else if (a[aMid] > b[bMid])
-		{
-			if (indexAEnd != indexAStar)
-			{
-				indexAEnd = aMid - 1;
-			}
-			if (indexBEnd != indexBStar)
-			{
-				indexBStar = bMid + 1;
-			}
-		}
-		else
-		{
-			if (indexAEnd != indexAStar)
-			{
-				indexAStar = aMid + 1;
-			}
-			if (indexBEnd != indexBStar)
-			{
-				indexBEnd = bMid - 1;
-			}
-
-
-		}
+		L = (LinkList)malloc(sizeof(LinkList));
+		L->next = NULL;
 	}
-	if (a[aMid] < b[bMid])
+	while (x != 9999)
 	{
-		return a[aMid];
+		node = (LNode *)malloc(sizeof(LNode));
+		node->value = x;
+		node->next = L->next;
+		L->next = node;
+		scanf_s("%d", &x);
 	}
-	else
+	return L;
+}
+
+void showLinkList(LinkList head)
+{
+	LinkList p = NULL;
+	p = head; 
+	if (head == NULL)
 	{
-		return b[bMid];
+		printf("head is NULL\n");
+	}
+	while (p=p->next)
+	{
+		printf("%d\t", p->value);
 	}
 }
+
 int main()
 {
-	int a[9] = { 0,0,0,0,0,0,0,0,0 };
-	int b[9] = { 2,4,6,8,10,12,14,16,18 };
-	printf("%d\n", getMid(a, b, 9));
-}
-
-void showArray(int as[], int length)
-{
-	for (int i = 0; i < length; i++)
-	{
-		printf("%d\t", as[i]);
-	}
+	LinkList head = NULL;
+	head = onCreate(head);
+	showLinkList(head);
 }
